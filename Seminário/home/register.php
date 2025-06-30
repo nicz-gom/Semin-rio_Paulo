5166c62c50f2becb6ca7810503f1f84d45f51dde
Nícolas <161242131+nicz-gom@users.noreply.github.com>
2025-06-29 23:55:16 -0300

    Commit 01

---
+++ Seminário/home/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <img src="/img/produtos.png">
+        <input type="text" name="nome" placeholder="Nome:" required>
+        <input type="password" name="senha" placeholder="Senha:" required>
+        <input type="password" name="confirmacao" placeholder="Confirme a Senha:" required>
+        <button>Criar</button>
+        <p>Tem uma conta?</p><a href="index.php">Entrar</a>
+    </form>
+</body>
+</html>
